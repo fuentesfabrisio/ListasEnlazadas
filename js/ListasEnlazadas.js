1151ab2c0dd1c8d675d661d3631abca1dc488295
fabrisio fuentes <jfabrsio@hotmail.com>
2019-12-16 18:06:42 -0600

    Creacion de los archivos base: HTML, CSS, JS
    Creacion de los archivos base: HTML, CSS, JS

---
+++ js/ListasEnlazadas.js
@@ -0,0 +1,34 @@
+class Node {
+    constructor(datos, siguiente){
+        this.datos = datos;
+        this.siguiente = siguiente;
+    }
+}
+
+class ListasEnlazadas{
+    constructor(){
+        this.cabeza = null;
+        this.tamaño = 0;
+    }
+
+    add(datos){
+        const newNode = new Node(datos, null);
+        if(!this.cabeza){
+            this.cabeza = newNode;
+        }else{
+            let actual = this.cabeza;
+            while(actual.next){
+                actual = actual.next;
+            }
+            actual.next = newNode;
+        }
+        this.tamaño++;
+    }
+
+
+}
+
+
+
+const testNode = new Node("test",null);
+console.log(testNode);
